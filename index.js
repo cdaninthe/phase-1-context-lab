1e518f09fe2b8b86b2058e00ba15230d9e65cc84
Cecilia Daninthe <cecilia.daninthe@gmail.com>
2022-03-02 12:17:42 -0500

    commit tree dc9e25e8a13061d5d28f606f695c775665ed7ec3

--- index.js
+++ index.js
@@ -1,4 +1,96 @@
 /* Your Code Here */
+function createEmployeeRecord(array){
+    return {
+        firstName: array[0],
+        familyName: array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+function createEmployeeRecords (arrOfArr) {
+    return arrOfArr.map(employee => {
+        return createEmployeeRecord(employee)
+    })   
+}
+
+function createTimeInEvent(dateStamp){
+    let obj = {
+        type: "TimeIn", 
+        hour: parseInt(dateStamp.slice(-4)), 
+        date: dateStamp.slice(0, 10)
+    }
+    this.timeInEvents.push(obj)
+    return this 
+}
+
+function createTimeOutEvent(dateStamp){
+    let obj = {
+        type: "TimeOut", 
+        hour: parseInt(dateStamp.slice(-4)), 
+        date: dateStamp.slice(0, 10)
+    }
+    this.timeOutEvents.push(obj)
+
+    return this   
+}
+
+function hoursWorkedOnDate(date){
+    let hours;
+    
+    for (let i=0; i<this.timeInEvents.length; i++){
+        if (this.timeInEvents[i].date === date){
+            if (this.timeOutEvents[i].date === date){
+                hours = this.timeOutEvents[i].hour - this.timeInEvents[i].hour
+            }
+        }
+    }
+
+    return hours/100
+}
+
+function wagesEarnedOnDate(date){
+    return (hoursWorkedOnDate.call(this, date)) * this.payPerHour
+}
+
+// function allWagesFor(){
+//     let allPay = [];
+//     let allDates = [];
+
+//     for (let i = 0; i < this.timeInEvents.length; i++){
+//         allDates.push(this.timeInEvents[i].date)
+//     }
+
+//     allDates.forEach(date => {
+//         allPay.push(wagesEarnedOnDate.call(this, date))
+//     });
+
+//     return allPay.reduce(( previousValue, currentValue ) => previousValue + currentValue)
+// }
+
+function findEmployeeByFirstName(srcArray, firstName){
+    return srcArray.find(employee => employee.firstName === firstName)
+}
+
+function calculatePayroll(arrOfERecordObj){
+    let payroll = [];
+    // console.log('arr...')
+    // console.log(arrOfERecordObj)
+    arrOfERecordObj.forEach(employee => {
+        // console.log('employee')
+        // console.log(employee)
+        payroll.push(allWagesFor.call(employee)) 
+    });
+    //console.log(payroll)
+    return payroll.reduce((previousValue, currentValue) => previousValue + currentValue, 0) - 1200
+}
+
+
+
+
+
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
@@ -20,4 +112,5 @@ const allWagesFor = function () {
 
     return payable
 }
+
 
